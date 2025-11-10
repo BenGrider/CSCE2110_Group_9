4fff22a08ad40686bdb5a2a3ea229c936a51cc76
Ben Grider <107779973+BenGrider@users.noreply.github.com>
2025-11-10 13:14:28 -0600

    Implement reading and printing flight data from file
    Added functionality to read from flight.txt and print its contents.

--- flightToADJMatrix.cpp
+++ flightToADJMatrix.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+int main() {
+  string line;
+  ofstream flightDirectory (flight.txt);
+  if(flightDirectory.is_open()) {
+    while(getline(flightDirectory,line)) {
+      cout << line << endl;
+    }
+    flightDirectory.close();
+  }
+  else { cout << "file is closed"}
+  
+  return 0;
+}; 
+
+
+
+
